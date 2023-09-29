195bf8e40b18472193c7164d7f443c2048922e87
Valeska Marques <valeskkitty@gmail.com>
2023-09-29 08:48:28 -0300

    Require das classes Pagamento

--- index.php
+++ index.php
@@ -5,44 +5,29 @@
   <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Exemplo 7</title>
+  <title>Exemplo 8</title>
 </head>
 <body>
-    <h1>PHP com POO - Exemplo 7</h1>
+    <h1>PHP com POO - Exemplo 8</h1>
     <hr>
 
     <h2>Assuntos abordados</h2>
 
     <ul>
-      <li>Propriedades e métodos estáticos</li>
-      <li>Acesso direto sem necessidade de objetos/instnacias</li>
-      <li>Uso do <code>self</code> para acesso (dentro da classe) aos recursos estáticos</li>
+      <li>Namespaces: agrupamento e organização de recursos (classes, funções, constantes)</li>
+      <li>Prevenção de conflitos entre classes de mesmo nome</li>
+      <li>Configurar e usar <code>namespaces</code> e <code>alias</code></li>
     </ul>
 
     <?php
-    require_once "src/PessoaFisica.php";
+    /*
+     * Só de fazer o require/importação das classes, já da erro no servidor
+     * devido a ter duas classes com o mesmo nome
+     */
+    require_once "src/fornecedores/Pagamento.php";
+    require_once "src/prestadores/Pagamento.php"
 
-    $cliente1 = new PessoaFisica();
-    $cliente1->setNome("Astrogildo");
-    $cliente1->setIdade(75);
 
-    $cliente2 = new PessoaFisica();
-    $cliente2->setNome("Enzo");
-    $cliente2->setIdade(20);
-
-    require_once "src/Utilitarios.php";
-    Utilitarios::obterData(); // acesso direto sem necessidade de instanciar a classe
     ?>
-
-    <h2>Atendimentos do dia: <?=Utilitarios::$dataAtual?></h2>
-
-    <h3>Cliente: <?=$cliente1->getNome()?></h3>
-    <p>Tipo de atendimento: <?=Utilitarios::definirAtendimento($cliente1->getIdade())?></p>
-
-    <hr>
-
-    <h3>Cliente: <?=$cliente2->getNome()?></h3>
-    <p>Tipo de atendimento: <?=Utilitarios::definirAtendimento($cliente2->getIdade())?></p>
-
 </body>
 </html>
